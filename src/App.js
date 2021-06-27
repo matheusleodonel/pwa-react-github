545453859ddf55eac8a8aa4680203f952f7ed35a
Matheus Leodonel <matheusleodonel17@gmail.com>
2021-06-27 12:48:24 -0300

    Configurada a rota para login; Login recebendo nome de usuário com API;

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import {
   BrowserRouter as Router,
   Switch,
   Route,
 } from "react-router-dom";
+import axios from 'axios';
 
 import Login from './Login/Login';
 import Home from './Home/Home';
@@ -14,18 +15,50 @@ import Following from './Following/Following';
 
 
 function App() {
+  const [apilist, setApilist] = useState(0);
+
+  /* CONST USERNAME */
+
+  const setUser = (valor) => {
+    axios
+      .get(
+        "https://api.github.com/users/" + valor
+      )
+      .then((res) => {
+        setApilist(res.data);
+        window.location.href = '/home'
+      })
+      .catch((error) => {
+        if(error.res){
+          console.log("errar é humano")
+        } else if(error.request){
+          console.log("request 3")
+        } else{
+          console.log("elseeee")
+        }
+        console.log("Isso não existe")
+      })
+    console.log(apilist);
+  }
+/* 
+  CRIAR FUNÇÃO DE REPOSITÓRIO
+*/
+  const handleClick = (event) => {
+
+    setUser(event.target.login.value);
+    event.preventDefault();
+  }
   return (
     <div className="App">
-      <button>Home</button>
       <Router>
         <Switch>
-          <Route path="/login">
-            <Login />
+          <Route exact path="/">
+            <Login handleClick={handleClick} />
           </Route>
         </Switch>
 
         <Switch>
-          <Route path="/">
+          <Route path="/home">
             <Home />
           </Route>
         </Switch>
@@ -52,4 +85,56 @@ function App() {
   );
 }
 
+
+
 export default App;
+
+/*function App {
+  constructor(){
+    super();
+    this.state = {
+      githubData: [],
+      textInput: ""
+    };
+  }
+  componentDidMount(){
+    axios
+      .get(
+        api.baseUrl + `https://api.github.com/` +
+        api.client_id + "&client_secret=" + api.client_secret
+      )
+      .then((res) => {
+        console.log("Infos da API", res);
+
+        this.setState({ githubData: res.data.items });
+      });
+
+
+      function handleClick () {
+          console.log(textInput);
+
+      }
+
+      const handleChange = (event) => {
+          this.setState({setTextInput:event.target.value});
+      }
+  }
+
+  render() {
+    const { githubData } = this.state;
+    //console.log("Infos do State", githubData);
+    return (
+      <div className="container App">
+        <div className="row">
+          {githubData.map((dataMap) => (
+            <div key={dataMap.id} className="col-md-12">
+              <Login item = {dataMap.name} />
+          </div>
+          ))}
+        </div>
+        <Login handleClick={handleClick} handleChange={handleChange}/>
+      </div>
+    )
+  }
+}
+*/
--- src/Login/Login.jsx
+++ src/Login/Login.jsx
@@ -1,8 +1,18 @@
 import React from 'react';
 
-const Login = () => {
-    return <p>Login</p>
-}
+const Login = (props) => {
+    const { handleClick } = props;  
+    return (
+        <div>
+            <form onSubmit={handleClick} method="post">
+            <input id="login" name="login" required placeholder="Usuário"></input>
+            <br></br>
+            <button type="submit">Entrar</button>
+            </form>
+        </div>
+    );
+};
+
 
 
 export default Login;
