88c954888747eabab75607a965abd7098dac52da
Matheus Leodonel <matheusleodonel17@gmail.com>
2021-07-03 21:08:33 -0300

    Add estrutura inicial da home

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,16 +1,37 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import { useHistory } from 'react-router';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
     const findUser = useSelector(state => state.user)
-    console.log(findUser)
+
+    let history = useHistory();
+
+    function exitApp() {
+        history.push("/");
+    }
+
     return (
         <div>
-            <h1>
-                Login: {findUser.login}
-                <br></br>
-                Avatar: {findUser.avatar_url}
-            </h1>
+            <div>
+                #{findUser.login}
+            </div>
+            <div>
+                <button onClick={() => exitApp()}>Sair</button>
+            </div>
+            <div>
+                <img src={findUser.avatar_url}></img>
+            </div>
+            <div>
+                <Link to="/followers">{findUser.followers} Seguidores</Link>
+                <Link to="/following">{findUser.following} Seguindo</Link>
+                <Link to="/repositories">{findUser.public_repos} Repos</Link>
+            </div>
+            <div>
+                <p>BIO</p>
+                <p>{findUser.bio}</p>
+            </div>
         </div>
     );
 };
